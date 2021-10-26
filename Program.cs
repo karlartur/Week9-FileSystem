509458d09e4933ab5fddada5cac4c6db1a4e39d9
karlartur <89841872+karlartur@users.noreply.github.com>
2021-10-26 14:04:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace FileSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"C:\Users\opilane\samples";
+            string userDirectory = "wishList";
+
+            string newdirectoryFullPath = @$"{rootDirectory}\{userDirectory}";
+            Console.WriteLine(newdirectoryFullPath);
+
+            Directory.CreateDirectory(newdirectoryFullPath);
+        }
+    }
+}
