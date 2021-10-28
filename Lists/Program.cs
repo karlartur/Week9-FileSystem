8ee9789705fe8d58ed7918c89427cf2b9375f906
karlartur <89841872+karlartur@users.noreply.github.com>
2021-10-28 09:10:01 +0300

    Add files via upload

---
+++ Lists/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myWishes = new List<string>();
+            myWishes.Add("New crocs");
+            myWishes.Add("new umbrella");
+            myWishes.Add("new coffee mug");
+            myWishes.Add("new ring");
+
+            //Console.WriteLine($"There are {myWishes.Count} items on your wish list.");
+            //Console.WriteLine(myWishes[0]);
+            //Console.WriteLine(myWishes[myWishes.Count - 1]);
+
+            foreach(string wish in myWishes)
+            {
+                Console.WriteLine($"Item on your wish list: {wish}");
+            }
+            Console.WriteLine("Enter a wish:");
+            string userWish = Console.ReadLine();
+
+            myWishes.Add(userWish);
+            foreach (string wish in myWishes)
+            {
+                Console.WriteLine($"Item on your wish list: {wish}");
+            }
+
+        }
+    }
+}
